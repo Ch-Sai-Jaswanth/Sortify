d3c1823541319d12c77b0e8ff7c4f6d4879c1c31
Ch-Sai-Jaswanth <98694038+Ch-Sai-Jaswanth@users.noreply.github.com>
2025-01-24 22:32:07 +0530

    Update Signup.js

--- claudyy/src/Components/Signup.js
+++ claudyy/src/Components/Signup.js
@@ -29,7 +29,7 @@ const Signup = () => {
     }
 
     try {
-        const url = 'http://localhost:5000/auth/signup';
+        const url = 'https://sortify-visualizer.vercel.app//auth/signup';
         const response = await fetch(url, {
           method: 'POST',
           headers: {
